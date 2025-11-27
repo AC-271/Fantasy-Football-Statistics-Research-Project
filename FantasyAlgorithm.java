f02822b658281078ec1a0885d4089866896b7bf2
AC-271 <smellybananajoe@hotmail.com>
2025-11-26 22:03:33 -0800

    Rename playerName to name and update constructor
    Refactor player attributes and constructor in FantasyAlgorithm.

--- FantasyAlgorithm.java
+++ FantasyAlgorithm.java
@@ -1,9 +1,23 @@
 // fantasy algorithm program code
 public class FantasyAlgorithm{
 
-  private String playerName;
+  private String name;
   private String position;
-  private double 
+  private double points;
+  private double matchup;
+  private matchup coaching;
+
+  public player(String n, String pos, double pts, double matchupDiff, double coach){
+    
+    name = n;
+    position = pos;
+    points = pts;
+    matchupDiff = matchup;
+    coaching = coach;
+    
+  }
+
+  
   
   public static void main(String[] args){
     
