8071fcbf385c6e4f5cb2ece3f41349392e464260
AC-271 <smellybananajoe@hotmail.com>
2025-11-26 21:30:00 -0800

    Create FantasyAlgorithm class skeleton
    Added initial structure for FantasyAlgorithm class.

--- FantasyAlgorithm.java
+++ FantasyAlgorithm.java
@@ -1,1 +1,13 @@
+// fantasy algorithm program code
+public class FantasyAlgorithm{
 
+  private String playerName;
+  private String position;
+  private double 
+  
+  public static void main(String[] args){
+    
+
+
+  }
+}
